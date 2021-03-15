53f89503e433b337cfb9fcedaf0c1b584f7804a4
Tomasz Krzysztoń <77742277+ForgetAnyway@users.noreply.github.com>
2021-03-15 18:19:53 +0100

    find method

--- codingchallenges.js
+++ codingchallenges.js
@@ -1,48 +1,42 @@
-// Starting Data
+// find method
 
-const dogAges = [5, 2, 4, 1, 15, 8, 3];
-const dogAges2 = [16, 6, 10, 5, 6, 1, 4];
-
-// Helpfull Function
-
-const toHumanAge = function(dogAge) {
-    if (dogAge <= 2) {
-        return (humanAge = dogAge * 2);
-    } else if (dogAge > 2) {
-        return (humanAge = 16 + dogAge * 4);
-    }
+const account1 = {
+    owner: "Jonas Schmedtmann",
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 2222,
 };
 
-//  Final Function
-
-const calcAverageHuman = function(dogAges) {
-    const humanAges = dogAges.map(toHumanAge);
-
-    const adultHumanAges = humanAges.filter(function(humanAge) {
-        return humanAge >= 18;
-    });
-
-    const average = adultHumanAges.reduce(
-        (acc, adultHumanAge) => acc + adultHumanAge,
-        0
-    );
-    console.log(average / adultHumanAges.length);
+const account2 = {
+    owner: "Jessica Davis",
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
 };
-calcAverageHuman(dogAges);
-calcAverageHuman(dogAges2);
 
-// Same function with using Chain and Arrow Function
+const account3 = {
+    owner: "Steven Thomas Williams",
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+};
 
-const calcAverageChain = (dogAges) =>
-    dogAges
-    .map((dogAge) => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
-    .filter((dogAge) => dogAge > 18)
-    .reduce(
-        (acc, adultHumanAge, i, arr) => acc + adultHumanAge / arr.length,
-        0
-    );
+const account4 = {
+    owner: "Sarah Smith",
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+};
 
-const avg1 = calcAverageChain(dogAges);
-const avg2 = calcAverageChain(dogAges2);
+const accounts = [account1, account2, account3, account4];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-console.log(avg1, avg2);
+const firstWithdrawal = movements.find((mov) => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find((acc) => acc.owner === "Jessica Davis");
+
+console.log(account);
